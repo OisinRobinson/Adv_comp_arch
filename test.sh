38ffb05f25e318652be9e1750a079898c8ca09d9
Oisin Robinson <oroisinrobinson9@gmail.com>
2020-02-24 22:55:33 +0000

    more

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 echo "test program"
 
-../build/ARM/gem5.opt ../configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=4kB --l1i_size=4kB --option="telecomm/adpcm/data/small.pcm" -c ../telecomm/CRC32/crc32arm
+../build/ARM/gem5.opt ../configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=4kB --l1i_size=4kB --option="../telecomm/adpcm/data/small.pcm" -c ../telecomm/CRC32/crc32arm
 
 mv ../m5out/stats.txt ./stats_moved.txt
 
