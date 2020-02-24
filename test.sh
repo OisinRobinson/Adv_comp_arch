1bb7857cece54c175dbc8bdb55958d937549885c
Oisin Robinson <oroisinrobinson9@gmail.com>
2020-02-24 23:24:42 +0000

    vx

--- test.sh
+++ test.sh
@@ -2,14 +2,14 @@
 
 echo "test program"
 
-../build/ARM/gem5.opt ../configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=4kB --l1i_size=4kB --option="../telecomm/adpcm/data/small.pcm" -c ../telecomm/CRC32/crc32arm
+./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=4kB --l1i_size=4kB --option="./telecomm/adpcm/data/small.pcm" -c ./telecomm/CRC32/crc32arm
 
-cp ../m5out/stats.txt ../Adv_comp_arch/stats_4_4.txt
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_4_4.txt
 
 
 
 ../build/ARM/gem5.opt ../configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=8kB --l1i_size=4kB --option="../telecomm/adpcm/data/small.pcm" -c ../telecomm/CRC32/crc32arm
 
-cp ../m5out/stats.txt ../Adv_comp_arch/stats_8_4.txt
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_8_4.txt
 
 echo "test completed"
