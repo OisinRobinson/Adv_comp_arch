e11b3119e8b233605d02a0ee75cb0a2be5bdee54
Oisin Robinson <oroisinrobinson9@gmail.com>
2020-02-24 22:27:43 +0000

    commit with previous instruction

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 #bin/bash
 
 echo "test program"
+
+./build/ARM/gem5.opt configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=4kB --l1i_size=4kB --option="telecomm/adpcm/data/small.pcm" -c telecomm/CRC32/crc32arm
+
+echo "test completed"
