298163aa8efb8d3f029f807690a5734f02b066db
Oisin Robinson <oroisinrobinson9@gmail.com>
2020-02-25 18:17:41 +0000

    Big files for second part

--- test.sh
+++ test.sh
@@ -2,30 +2,39 @@
 
 echo "test program"
 
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=4 --cacheline_size=128 --caches --l1d_size=16kB --l1i_size=16kB --option=./telecomm/adpcm/data/small.pcm -c ./telecomm/CRC32/crc32
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=8 --cacheline_size=16 --caches --l1d_size=2kB --l1i_size=2kB --option=./telecomm/adpcm/data/large.pcm -c ./telecomm/CRC32/crc32
 
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_No3_4.txt
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_2_8_16.txt
 
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=8 --cacheline_size=128 --caches --l1d_size=16kB --l1i_size=16kB --option=./telecomm/adpcm/data/small.pcm -c ./telecomm/CRC32/crc32
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=8 --cacheline_size=32 --caches --l1d_size=2kB --l1i_size=2kB --option=./telecomm/adpcm/data/large.pcm -c ./telecomm/CRC32/crc32
 
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_No3_8.txt
+cp ./m5out/stats.txt ./Adv_comp_arch/stats2_8_32.txt
 
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=16 --cacheline_size=128 --caches --l1d_size=16kB --l1i_size=16kB --option=./telecomm/adpcm/data/small.pcm -c ./telecomm/CRC32/crc32
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=8 --cacheline_size=64 --caches --l1d_size=2kB --l1i_size=2kB --option=./telecomm/adpcm/data/large.pcm -c ./telecomm/CRC32/crc32
 
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_No3_16.txt
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_2_8_64.txt
 
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=DerivO3CPU --l1d_assoc=4 --cacheline_size=128 --caches --l1d_size=16kB --l1i_size=16kB --option=./telecomm/adpcm/data/small.pcm -c ./telecomm/CRC32/crc32
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=8 --cacheline_size=128 --caches --l1d_size=2kB --l1i_size=2kB --option=./telecomm/adpcm/data/large.pcm -c ./telecomm/CRC32/crc32
 
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_o3_4.txt
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_2_8_128.txt
 
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=DerivO3CPU --l1d_assoc=8 --cacheline_size=128 --caches --l1d_size=16kB --l1i_size=16kB --option=./telecomm/adpcm/data/small.pcm -c ./telecomm/CRC32/crc32
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=8 --cacheline_size=16 --caches --l1d_size=4kB --l1i_size=4kB --option=./telecomm/adpcm/data/large.pcm -c ./telecomm/CRC32/crc32
 
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_o3_8.txt
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_4_8_16.txt
 
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=DerivO3CPU --l1d_assoc=16 --cacheline_size=128 --caches --l1d_size=16kB --l1i_size=16kB --option=./telecomm/adpcm/data/small.pcm -c ./telecomm/CRC32/crc32
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=8 --cacheline_size=32 --caches --l1d_size=4kB --l1i_size=4kB --option=./telecomm/adpcm/data/large.pcm -c ./telecomm/CRC32/crc32
 
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_o3_16.txt
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_4_8_32.txt
 
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=8 --cacheline_size=64 --caches --l1d_size=4kB --l1i_size=4kB --option=./telecomm/adpcm/data/large.pcm -c ./telecomm/CRC32/crc32
+
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_4_8_64.txt
+
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=8 --cacheline_size=128 --caches --l1d_size=4kB --l1i_size=4kB --option=./telecomm/adpcm/data/large.pcm -c ./telecomm/CRC32/crc32
+
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_4_8_128.txt
+
+.
 echo "test 32"
 
 
