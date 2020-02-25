59238c18a88f230b69139090d3c03ca27b4d3498
Oisin Robinson <oroisinrobinson9@gmail.com>
2020-02-25 15:51:39 +0000

    changes to part B for crc 32

--- test.sh
+++ test.sh
@@ -2,133 +2,53 @@
 
 echo "test program"
 
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=2kB --l1i_size=2kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_2_2.txt
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=4kB --l1i_size=2kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_4_2.txt
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=8kB --l1i_size=2kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_8_2.txt
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=16kB --l1i_size=2kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_16_2.txt
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=32kB --l1i_size=2kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_32_2.txt
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=64kB --l1i_size=2kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_64_2.txt
-
-echo "test 2"
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=2kB --l1i_size=4kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_2_4.txt
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=4kB --l1i_size=4kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_4_4.txt
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=8kB --l1i_size=4kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_8_4.txt
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=16kB --l1i_size=4kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_16_4.txt
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=32kB --l1i_size=4kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_32_4.txt
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=64kB --l1i_size=4kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_64_4.txt
-
-echo "test 4"
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=2kB --l1i_size=8kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_2_8.txt
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=4kB --l1i_size=8kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_4_8.txt
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=8kB --l1i_size=8kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_8_8.txt
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=16kB --l1i_size=8kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_16_8.txt
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=32kB --l1i_size=8kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_32_8.txt
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=64kB --l1i_size=8kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_64_8.txt
-
-echo "test 8"
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=2kB --l1i_size=16kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_2_16.txt
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=4kB --l1i_size=16kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=4 --cacheline_size=16 --caches --l1d_size=16kB --l1i_size=16kB --option=./telecomm/adpcm/data/small.pcm -c ./telecomm/CRC32/crc32
 
 cp ./m5out/stats.txt ./Adv_comp_arch/stats_4_16.txt
 
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=8kB --l1i_size=16kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_8_16.txt
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=16kB --l1i_size=16kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_16_16.txt
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=32kB --l1i_size=16kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_32_16.txt
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=64kB --l1i_size=16kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_64_16.txt
-
-echo "test 16"
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=2kB --l1i_size=32kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_2_32.txt
-
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=4kB --l1i_size=32kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=4 --cacheline_size=32 --caches --l1d_size=16kB --l1i_size=16kB --option=./telecomm/adpcm/data/small.pcm -c ./telecomm/CRC32/crc32
 
 cp ./m5out/stats.txt ./Adv_comp_arch/stats_4_32.txt
 
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=8kB --l1i_size=32kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=4 --cacheline_size=64 --caches --l1d_size=16kB --l1i_size=16kB --option=./telecomm/adpcm/data/small.pcm -c ./telecomm/CRC32/crc32
+
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_4_64.txt
+
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=4 --cacheline_size=128 --caches --l1d_size=16kB --l1i_size=16kB --option=./telecomm/adpcm/data/small.pcm -c ./telecomm/CRC32/crc32
+
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_4_128.txt
+
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=8 --cacheline_size=16 --caches --l1d_size=16kB --l1i_size=16kB --option=./telecomm/adpcm/data/small.pcm -c ./telecomm/CRC32/crc32
+
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_8_16.txt
+
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=8 --cacheline_size=32 --caches --l1d_size=16kB --l1i_size=16kB --option=./telecomm/adpcm/data/small.pcm -c ./telecomm/CRC32/crc32
 
 cp ./m5out/stats.txt ./Adv_comp_arch/stats_8_32.txt
 
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=16kB --l1i_size=32kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=8 --cacheline_size=64 --caches --l1d_size=16kB --l1i_size=16kB --option=./telecomm/adpcm/data/small.pcm -c ./telecomm/CRC32/crc32
+
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_8_64.txt
+
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=8 --cacheline_size=128 --caches --l1d_size=16kB --l1i_size=16kB --option=./telecomm/adpcm/data/small.pcm -c ./telecomm/CRC32/crc32
+
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_8_128.txt
+
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=16 --cacheline_size=16 --caches --l1d_size=16kB --l1i_size=16kB --option=./telecomm/adpcm/data/small.pcm -c ./telecomm/CRC32/crc32
+
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_16_16.txt
+
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=16 --cacheline_size=32 --caches --l1d_size=16kB --l1i_size=16kB --option=./telecomm/adpcm/data/small.pcm -c ./telecomm/CRC32/crc32
 
 cp ./m5out/stats.txt ./Adv_comp_arch/stats_16_32.txt
 
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=32kB --l1i_size=32kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=16 --cacheline_size=64 --caches --l1d_size=16kB --l1i_size=16kB --option=./telecomm/adpcm/data/small.pcm -c ./telecomm/CRC32/crc32
 
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_32_32.txt
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_16_64.txt
 
-./build/ARM/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=64kB --l1i_size=32kB --option="./automotive/susan/input_small.pgm -s" -c ./automotive/susan/susanarm
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=16 --cacheline_size=128 --caches --l1d_size=16kB --l1i_size=16kB --option=./telecomm/adpcm/data/small.pcm -c ./telecomm/CRC32/crc32
 
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_64_32.txt
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_16_128.txt
 
 echo "test 32"
 
